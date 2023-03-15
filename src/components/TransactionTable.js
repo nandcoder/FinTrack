bb3cadf2aa0fc1a97666fa1a379dd48e0fcff296
Nand Raj <nandraj2001@gmail.com>
2023-03-16 02:03:45 +0530

    updated table with dynamic data

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -1,8 +1,35 @@
 import Table from 'react-bootstrap/Table';
 import Badge from 'react-bootstrap/Badge';
 import data from '../assets/TransactionData';
+import { db } from '../utils/firebase';
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from './Authentication/AuthProvider';
 
 function StripedRowExample() {
+    const { user } = useContext(AuthContext);
+    const [request, setRequest] = useState(true);
+    const [transctions, setTransctions] = useState([]);
+
+
+    useEffect(() => {
+        let temp = [];
+        db.collection("transactions")
+            .where("involved", "array-contains", user.uid)
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    // doc.data() is never undefined for query doc snapshots
+                    console.log(doc.id, " => ", doc.data());
+                    temp.push(doc.data());
+                });
+            })
+            .catch((error) => {
+                console.log("Error getting documents: ", error);
+            })
+            .finally(() => setTransctions(temp))
+        console.log(user.uid);
+        // console.log(data);
+    }, [request]);
 
 
     return (
@@ -27,7 +54,7 @@ function StripedRowExample() {
             </thead>
             <tbody>
 
-                {data.map((row) => (
+                {transctions?.map((row) => (
                     <tr>
                         <td><div class="d-flex align-items-center">
 
@@ -46,37 +73,33 @@ function StripedRowExample() {
                         <td>{row.date}</td>
                         <td>
 
-               
-                            
-                            
-                            
-                            
+                            {row.paidBy === user.uid ? (
+                                <div>
+                                    <Badge pill bg="success">
+                                        {row.status} Pending
+                                    </Badge>
+                                </div>
 
-
-                            <div>
-                                <Badge pill bg="success">
-                                    {row.status}
-                                </Badge>
-                            </div>
-                            {/* <div>
-                                <Badge pill bg="secondary">
-                                        {row.status}
-                                </Badge>
-                            </div> */}
+                            ) : (
+                                <div>
+                                    <Badge pill bg="secondary">
+                                        {row.status} Pending
+                                    </Badge>
+                                </div>
+                            )}
 
                         </td>
 
-            <td>{row.amount}</td>
+                        <td>{row.amount}</td>
 
-            <td>
-                <button type="button" class="btn btn-primary btn-sm">Edit</button>
+                        <td>
+                            <button type="button" class="btn btn-primary btn-sm">Edit</button>
 
-            </td>
-        </tr>
-    ))
-}
-            </tbody >
-        </Table >
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
     );
 }
 
