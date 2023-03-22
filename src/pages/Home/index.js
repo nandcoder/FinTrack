2c8473f6b449d05672fc3394e27b5d5af12a5f64
Nand Raj <nandraj2001@gmail.com>
2023-03-22 23:55:28 +0530

    changes in Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,45 +1,75 @@
+import { useContext, useEffect, useState } from "react";
+import { Button, Container } from "react-bootstrap";
 import Card from "../../components/Card";
-// import { useState } from "react";
-import { Container } from "react-bootstrap";
-import UserTable from "../../components/UserTable";
-import TransactionTable from "../../components/TransactionTable";
+import AddGroup from "./Sections/AddGroup";
+import GroupCard from "./Sections/GroupCard";
+import { db } from "../../utils/firebase";
+import { AuthContext } from "../../components/Authentication/AuthProvider";
 
 const Home = () => {
+    const { user } = useContext(AuthContext);
+    const [groups, setGroups] = useState([{ name: "123" }]);
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    useEffect(() => {
+        let temp = [];
+        db.collection("groups")
+            .where("members", "array-contains", user.uid)
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    // doc.data() is never undefined for query doc snapshots
+                    temp.push(doc.data());
+                });
+            })
+            .catch((error) => {
+                console.log("Error getting documents: ", error);
+            })
+            .finally(() => {
+                setGroups(temp)
+            });
+
+    }, [user]);
+
     // const [spent, setSpent] = useState(0);
     // const [remain, setRemain] = useState(0);
-    const spentLink = "/";
-    const remainLink = "/";
+    // const spentLink = "/";
+    // const remainLink = "/";
 
-    // const handleClick = () => {
-    //   console.log(user.uid);
-    //   console.log(user.email);
-    //   console.log(user.displayName);
-    // };
     return (
         <>
             <Container id="feedback">
                 <Card
                     className="fbcard"
-                    head="Total Amount Spent"
-                    // text={spent}
-                    link={spentLink}
+                    head="Total Expenditure"
+                    text="10000"
+                    link="/transaction"
                 />
                 <Card
                     className="fbcard"
-                    head="Budget Remaining"
-                    // text={remain}
-                    link={remainLink}
+                    head="My Expenses"
+                    text="3000"
+                    link="/transaction"
+                />
+                <Card
+                    className="fbcard"
+                    head="Amount Pending"
+                    text="800"
+                    link="/user"
                 />
             </Container>
             <br />
             <br />
             <Container id="content">
-                <UserTable />
-            </Container>
-            <br />
-            <br />
-            <Container id="content">
-                <TransactionTable />
+                <div className="groups-wrapper">
+                    <Button onClick={handleShow}>+</Button>
+                    {show && <AddGroup show={show} handleClose={handleClose} />}
+                    {groups?.map((group, key) => (
+                        <GroupCard name={group.title} key={key} />
+                    ))}
+                </div>
             </Container>
         </>
     );
