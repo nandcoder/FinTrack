a000abb3ea89e40cae28ecaba66fa5100b6896a2
Nand Raj <nandraj2001@gmail.com>
2023-04-03 05:28:15 +0530

    updated users page

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -3,54 +3,78 @@ import Table from 'react-bootstrap/Table';
 // import data from '../assets/UserData';
 import { db } from '../utils/firebase';
 import { AuthContext } from './Authentication/AuthProvider';
+import { Badge, Button, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { LaunchRounded } from '@mui/icons-material';
 
-function UserTable() {
+function UserTable({ users, transactions }) {
     const { user } = useContext(AuthContext);
-    // const [userData, setUserData] = useState([])
-    // const [request, setRequest] = useState(true)
-    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true)
+    // const [balances, setBalances] = useState([]);
     useEffect(() => {
-        let temp = [];
-        db.collection("transactions")
-            .where("involved", "array-contains", user.uid)
-            .get()
-            .then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    // doc.data() is never undefined for query doc snapshots
-                    console.log(doc.id, " => ", doc.data());
-                    temp.push(doc.data());
-                });
+        transactions.forEach(transaction => {
+            transaction.data.involved.forEach((personId) => {
+                console.log(users[personId]);
+                if (personId === user.uid) {
+                }
+                else {
+                    if (transaction.data.paidBy === user.uid) {
+                        users[personId].amount += transaction.data.amount / transaction.data.involved.length
+                    } else {
+                        users[personId].amount -= transaction.data.amount / transaction.data.involved.length
+                    }
+                }
             })
-            .catch((error) => {
-                console.log("Error getting documents: ", error);
-            })
-            .finally(() => {
-                setData(temp);
-            })
+        })
+        setLoading(false)
     }, [user]);
     return (
-        <Table striped>
+        <Table style={{ fontSize: '1.5rem' }} striped>
             <thead>
                 <tr>
                     <th>Name</th>
-                    {/* <th>Date</th> */}
-                    <th>Category</th>
-                    <th>Amount</th>
                     <th>Status</th>
+                    <th>Amount</th>
                     <th>Details</th>
                 </tr>
             </thead>
             <tbody>
-                {data.map((row, key) => (
-                    <tr key={key}>
-                        <td>{row.name}</td>
-                        {/* <td>{row.date}</td> */}
-                        <td>{row.category}</td>
-                        <td>{row.amount}</td>
-                        <td>{row.status}</td>
-                        <td>{row.details}</td>
-                    </tr>
-                ))}
+                {!loading && Object.keys(users)?.map((row, key) => {
+                    console.log("ROW", users[row])
+                    if (users[row].userId !== user.uid) {
+                        const red = {
+                            50: '#ffebee', 100: '#ffcdd2', 200: '#ef9a9a', 300: '#e57373', 400: '#ef5350', 500: '#f44336', 600: '#e53935', 700: '#d32f2f', 800: '#c62828', 900: '#b71c1c', A100: '#ff8a80', A200: '#ff5252', A400: '#ff1744', A700: '#d50000',
+                        }
+                        return (
+                            <tr key={key}>
+                                <td>{users[row].name}</td>
+                                {/* <td>{users[row].date}</td> */}
+                                <td>
+                                    {users[row].amount >= 0 ? (
+                                        <Badge colorScheme='green'>Lent</Badge>
+                                    ) : (
+                                        <Badge colorScheme='red'>Borrowed</Badge>
+                                    )}
+                                </td>
+                                <td>
+                                    {users[row].amount >= 0 ? (
+                                        <Text as={'b'} color='green'>{users[row].amount}</Text>
+                                    ) : (
+                                        <Text as={'b'} color='red'>{users[row].amount}</Text>
+                                    )}
+                                </td>
+                                <td>
+                                    <Link to={`/transaction/user/${row}`}>
+                                        <Button colorScheme='messenger'>
+                                            Details
+                                            <LaunchRounded />
+                                        </Button>
+                                    </Link>
+                                </td>
+                            </tr>
+                        )
+                    }
+                })}
             </tbody>
         </Table>
     );
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,180 +1,88 @@
-import UserTable from "../../components/UserTable"
-import { Container } from "react-bootstrap";
-import { Heading } from "@chakra-ui/react";
 import { useContext, useEffect, useState } from "react";
-import { db } from "../../utils/firebase";
+import { Container } from "react-bootstrap";
+import { CircularProgress, Heading } from "@chakra-ui/react";
 import { AuthContext } from "../../components/Authentication/AuthProvider";
+import { db } from "../../utils/firebase";
+import UserTable from "../../components/UserTable"
 const User = () => {
     const { user } = useContext(AuthContext);
     const [transactions, setTransactions] = useState([]);
-    const [users, setUsers] = useState([{ id: "uniqueId", name: 'user1', amount: 250, status: 'pending' }]);
+    const [users, setUsers] = useState({});
     const [friends, setFriends] = useState([]);
-    const [currPerson, setCurrPerson] = useState({})
-    const userBal = [];
-    const updateAmount = (transaction, personId) => {
-        console.log('calc', transactions);
-        const final = {
-            id: "6JBeu8XiwymcMFvj2VOZ",
-            data: {
-                involved: [
-                    "7tpEtNLHhURSQ2LmmmngVNwrClp1",
-                    "MVAdkWFkFqcQLfZiHl6PBPKqwOu2"
-                ],
-                status: "pending",
-                groupId: "KZoqHaGSVNkuq0QQovow",
-                title: "puri trip",
-                day: "2",
-                groupTitle: "Grp2",
-                amount: 500,
-                desc: "ssample desc",
-                paidBy: {
-                    userId: "7tpEtNLHhURSQ2LmmmngVNwrClp1",
-                    name: "NAND RAJ",
-                    email: "nandraj2001@gmail.com"
-                }
-            }
-        }
-
-        const currBal = () => {
-            const temp = final.data.amount / final.data.involved.length;
-            if (final.data.paidBy.userId === user.uid) {
-                return temp
-            } else {
-                return 0 - temp;
-            }
-        }
-        // users.forEach((currUser) => {
-        //     if (currUser.id === user.uid) {
-
-        //     }
-        // })
-
-
-
-
-
-
-
-
-
-    }
-
-    const handleNewPerson = (transaction, personId) => {
-        setFriends((prevFriends) => [...prevFriends, personId])
-        return getUserById(transaction, personId);
-    }
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
-        const arr = [];
+        const temp = [];
+        const tempFriends = []
         db.collection("transactions")
             .where("involved", "array-contains", user.uid)
             .get()
             .then((querySnapshot) => {
                 querySnapshot.forEach((doc) => {
-                    // db.collection("users")
-                    //     .where("userId", "==", doc.data().paidBy)
-                    //     .get()
-                    //     .then((querySnapshot) => {
-                    //         querySnapshot.forEach((payer) => {
-                    //             // payer.data() is never undefined for query payer snapshots
-                    //             // console.log(payer.id, " => ", payer.data());
-                    //             arr.push(payer.data());
+                    console.log(doc.id, " => ", doc.data());
+                    const id = doc.id;
+                    const data = doc.data();
+                    const finalTransaction = {
+                        id,
+                        data,
+                    }
+                    data.involved.forEach(personId => {
+                        if (tempFriends.includes(personId)) { }
+                        else tempFriends.push(personId)
+                    });
+                    temp.push(finalTransaction);
+                })
+            })
+            .catch((error) => {
+                console.log("Error getting documents: ", error);
+            })
+            .finally(() => {
+                setFriends(tempFriends);
+                setTransactions(temp);
+            });
 
-                    //             const id = doc.id;
-                    //             const docData = doc.data();
-                    //             docData.paidBy = payer.data();
-                    //             const finalTransaction = {
-                    //                 id,
-                    //                 data: docData,
-                    //             }
-                    //             console.log('final', finalTransaction);
-                    //             calculateAmount(finalTransaction)
-                    //             // arr.push(finalTransaction);
-                    //             // temp.push(finalTransaction);
-                    //             setTransactions((prevTransactions) => [...prevTransactions, finalTransaction])
-                    //             console.log(users);
-                    //         });
 
-                    //     })
-                    //     .catch((error) => {
-                    //         console.log("Error getting documents: ", error);
-                    //     })
-
+    }, [user]);
+    useEffect(() => {
+        const temp = {}
+        if (friends.length) db.collection("users")
+            .where("userId", "in", friends)
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
                     // doc.data() is never undefined for query doc snapshots
                     console.log(doc.id, " => ", doc.data());
                     const id = doc.id;
                     const data = doc.data();
-                    data.involved.forEach((personId) => {
-                        if (personId === user.uid) {
-
-                        }
-                        else if (friends.includes(personId)) {
-                            updateAmount(data, personId);
-                        }
-                        else {
-                            handleNewPerson(data, personId);
-                            // getUserById(person)
-                            // // friends.push(person)
-                            // setFriends((prevFriends) => [...prevFriends, person])
-                        }
-                    })
-                    // temp.push(doc.data());
+                    const dataWithAmt = { ...data, amount: 0 };
+                    temp[data.userId] = dataWithAmt;
+                    // setUsers((prevUsers) => {
+                    //     return { ...prevUsers, [id]: dataWithAmt, }
+                    // })
                 });
             })
             .catch((error) => {
                 console.log("Error getting documents: ", error);
             })
             .finally(() => {
-                // setTransactions(arr)
-                console.log(transactions);
-                // console.log("arr", arr);
-                // console.log("temp", temp);
-                // setUsers(arr)
-            });
-
-
-    }, [user]);
-    const getUserById = async (transaction, id) => {
-        let data;
-        let temp = []
-        return db.collection("users")
-            .where("userId", "==", id)
-            .get()
-            .then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    // doc.data() is never undefined for query doc snapshots
-                    console.log(doc.id, " => ", doc.data());
-                    temp.push(doc.data());
-                    data = doc.data()
-                    const amountRequired = transaction.amount / transaction.involved.length;
-                    data.amount = amountRequired;
-                    setCurrPerson(data);
-                    const finalUser = {
-                        id: doc.id,
-                        data: doc.data(),
-                    }
-                });
+                setUsers(temp)
+                setLoading(false)
             })
-            .catch((error) => {
-                console.log("Error getting documents: ", error);
-            })
-            .finally(() => { return currPerson })
-    }
-    // useEffect(() => {
-    //     calculateAmount();
-    // }, [currPerson]);
-
+    }, [friends]);
     return (
         <Container>
             <Heading margin={'2%'}>Users</Heading>
-            {transactions?.map((transaction) => (
+            {loading && <CircularProgress left={"50%"} top={"50%"} isIndeterminate size='100px' thickness='4px' />}
+            {/* {transactions?.map((transaction) => (
                 <div>
                     <p>id:- {transaction.id}</p>
                     <p>title:- {transaction.data.title} </p>
                     <p>date:- {transaction.data.day} </p>
                     <p>payer:- {transaction.data.paidBy.name} </p>
                 </div>
-            ))}
-            <UserTable data={users} />
+            ))} */}
+            {!loading && transactions.length !== 0 && Object.keys(users).length !== 0 && (
+                <UserTable users={users} transactions={transactions} />
+            )}
         </Container>
     );
 };
