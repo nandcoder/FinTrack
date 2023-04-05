ca9a9b8f010fe8c5d3ed2186ffb3eb837a0f1469
Nand Raj <nandraj2001@gmail.com>
2023-04-05 14:27:05 +0530

    server requests reduced

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,6 +8,78 @@ import { AuthContext } from "../../components/Authentication/AuthProvider";
 import AddButton from "../../components/AddButton";
 import Loader from "../../components/Loader";
 
+// export const DataContext = createContext();
+// export const DataProvider = ({ children }) => {
+//     const { user } = useContext(AuthContext);
+//     const [currentGroup, setCurrentGroup] = useState('');
+//     const [transactions, setTransactions] = useState([]);
+//     const [users, setUsers] = useState({});
+//     const [friends, setFriends] = useState([]);
+//     useEffect(() => {
+//         const temp = [];
+//         const tempFriends = []
+//         db.collection("transactions")
+//             .where("involved", "array-contains", user.uid)
+//             .get()
+//             .then((querySnapshot) => {
+//                 querySnapshot.forEach((doc) => {
+//                     // console.log(doc.id, " => ", doc.data());
+//                     const id = doc.id;
+//                     const data = doc.data();
+//                     const finalTransaction = {
+//                         id,
+//                         data,
+//                     }
+//                     data.involved.forEach(personId => {
+//                         if (tempFriends.includes(personId)) { }
+//                         else tempFriends.push(personId)
+//                     });
+//                     temp.push(finalTransaction);
+//                 })
+//             })
+//             .catch((error) => {
+//                 console.log("Error getting documents: ", error);
+//             })
+//             .finally(() => {
+//                 setFriends(tempFriends);
+//                 setTransactions(temp);
+//             });
+
+
+//     }, [user]);
+//     useEffect(() => {
+//         const temp = {}
+//         if (friends.length) db.collection("users")
+//             .where("userId", "in", friends)
+//             .get()
+//             .then((querySnapshot) => {
+//                 querySnapshot.forEach((doc) => {
+//                     // doc.data() is never undefined for query doc snapshots
+//                     // console.log(doc.id, " => ", doc.data());
+//                     const data = doc.data();
+//                     const dataWithAmt = { ...data, amount: 0 };
+//                     temp[data.userId] = dataWithAmt;
+//                     // setUsers((prevUsers) => {
+//                     //     return { ...prevUsers, [id]: dataWithAmt, }
+//                     // })
+//                 });
+//             })
+//             .catch((error) => {
+//                 console.log("Error getting documents: ", error);
+//             })
+//             .finally(() => {
+//                 setUsers(temp)
+//                 // setLoading(false)
+//             })
+//     }, [friends]);
+
+//     return (
+//         <DataContext.Provider value={{ transactions, setTransactions, users, setUsers, currentGroup, setCurrentGroup }} >
+//             {children}
+//         </DataContext.Provider>
+//     )
+// }
+
 const Home = () => {
     const { user } = useContext(AuthContext);
     const [groups, setGroups] = useState([]);
